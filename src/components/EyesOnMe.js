d8a0d59f11bbabe84e707fc0d7065879f3dc4bc9
DOROTHY CHEPKOECH <chepkoechdorothy209@gmail.com>
2025-04-12 21:06:42 +0300

    Add changes made

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,16 @@
 // Code EyesOnMe Component Here
+import React from "react";
+function EyesOnMe(){
+    const HandleFocus = () =>{
+        console.log('Good!');
+    }
+    const HandleBlur = () =>{
+        console.log('Hey! Eyes on me!');
+    }
+    return(
+        <div>
+            <button onFocus={HandleFocus}onBlur={HandleBlur}>Eyes On Me</button>
+        </div>
+    )
+}
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,13 @@
 // Code Keypad Component Here
-
+import React from "react";
 function Keypad (){
+    const handleChange = (event)=>{
+        console.log('Entering password...')
+    }
     return (
-        <div></div>
+        <div>
+            <input type="password" placeholder="Enter password"onChange={handleChange}/>
+        </div>
     )
 }
 
